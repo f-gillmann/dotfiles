b7e4b2059b02b2feda9efdbc1a8fdd93b77d6965
Florian Gillmann <mail@florian-gillmann.com>
2025-01-29 10:30:43 +0100

    Update get.sh script with colors and improve output to shell

--- get.sh
+++ get.sh
@@ -5,35 +5,46 @@
 #---\ | https://github.com/f-gillmann/dotfiles |  \--#
 #----\|  --  *      ---   **   ---      *  --  |   \-#
 
+RESET_COLOR="\e[0m"
+COLOR="\e[36m"
+LIGHT_COLOR="\e[96m"
+UNDERLINE="\e[4m"
+NEWLINE="\n"
+PREFIX="${COLOR}\$${RESET_COLOR}/${LIGHT_COLOR}>${RESET_COLOR}"
+
+printf "$PREFIX Downloading files from ${LIGHT_COLOR}${UNDERLINE}https://github.com/f-gillmann/dotfiles${RESET_COLOR}.$NEWLINE"
+
 # Check if the f-gillmann-dots directory exists
 if [ -d "f-gillmann-dots" ]; then
-  read -p "\$/> f-gillmann-dots already exists. Do you want to overwrite it? [y/N]: " overwrite
+  printf "$PREFIX"
+  read -r -p " f-gillmann-dots already exists. Do you want to overwrite it? [y/N]: " overwrite
   if [[ $overwrite =~ ^[Yy]$ ]]; then
     # Remove existing directory
     rm -rf f-gillmann-dots
   else
-    printf "\$/> Exiting...\n"
+    printf "$PREFIX Exiting...$NEWLINE"
     exit 1
   fi
 fi
 
 # Download the repository from github
 TEMP_DIR=$(mktemp -d) &&
-curl -L -o master.zip "https://github.com/f-gillmann/dotfiles/archive/refs/heads/master.zip" &&
-unzip master.zip -d $TEMP_DIR &&
+curl -sS -L -o master.zip "https://github.com/f-gillmann/dotfiles/archive/refs/heads/master.zip" &&
+unzip -qq master.zip -d $TEMP_DIR &&
 mkdir f-gillmann-dots &&
 mv $TEMP_DIR/dotfiles-master/* f-gillmann-dots/ &&
 rm -rf $TEMP_DIR master.zip &&
 
-printf "\$/> Dotfiles have been downloaded and extracted into f-gillmann-dots."
+printf "$PREFIX Dotfiles have been downloaded and extracted into f-gillmann-dots.$NEWLINE"
 
 # Prompt if we wanna run the install script
-read -p "\$/> Do you want to run the install script now? [y/N]: " overwrite
-if [[ $overwrite =~ ^[Yy]$ ]]; then
+printf "$PREFIX"
+read -r -p " Do you want to run the install script now? [y/N]: " overwrite
+if [[ $overwrite =~ ^[yY]$ ]]; then
   cd ./f-gillmann-dots &&
   chmod +x ./install.sh
   bash ./install.sh
 else
-  printf "\$/> Exiting...\n"
+  printf "$PREFIX Exiting...$NEWLINE"
   exit 1
 fi
