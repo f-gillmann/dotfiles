8ddb357ca1cb8f53b8b23a1a58c4689f4f82c0f9
Florian Gillmann <mail@florian-gillmann.com>
2025-01-29 10:44:55 +0100

    Supress grep error messages

--- install.sh
+++ install.sh
@@ -39,13 +39,13 @@ print_ascii_art $COLOR $LIGHT_COLOR
 #--------------#
 
 # Check if we're on an arch system
-if ! grep -qE '(ID=arch|ID_LIKE=arch)' /etc/*-release; then
+if ! grep -qsE '(ID=arch|ID_LIKE=arch)' /etc/*-release; then
   printf "$PREFIX You are not running an Arch or Arch-based Linux distribution.$NEWLINE"
   exit 1
 fi
 
 # Check if we're on an arch-based system
-if ! grep -qE '(ID_LIKE=arch)' /etc/*-release; then
+if ! grep -qsE '(ID_LIKE=arch)' /etc/*-release; then
   printf "$PREFIX Running on an Arch-based distribution is not officially supported${$NEWLINE}${PREFIX}"
   read -r -p " Do you wish to proceed with installation? [y/N] " response
 
