35c344c2fca448e6e81436811ae22fa705ee1723
Florian Gillmann <mail@florian-gillmann.com>
2025-10-08 21:29:06 +0000

    handle zsh files differently in install script

--- scripts/install_dotfiles.sh
+++ scripts/install_dotfiles.sh
@@ -10,7 +10,7 @@ install_dotfiles() {
     cd "$rice_dir"
     printf "$PREFIX Installing dotfiles to $HOME...$NEWLINE"
     
-    local stow_pkgs="hyprland kitty matugen quickshell walker wallpapers wallust waypaper zsh"
+    local stow_pkgs="hyprland kitty matugen quickshell walker wallpapers wallust waypaper"
     
     # Handle hyprcursor installation manually
     printf "$PREFIX Installing hyprcursor themes...$NEWLINE"
@@ -31,58 +31,67 @@ install_dotfiles() {
     
     printf "$PREFIX Hyprcursor themes installation completed.$NEWLINE"
     
-    # Check for existing .zshrc and handle it interactively
-    if [ -f "$HOME/.zshrc" ]; then
-        printf "$PREFIX Existing .zshrc found at $HOME/.zshrc$NEWLINE"
-        printf "$PREFIX What would you like to do?$NEWLINE"
-        printf "  1) Backup existing .zshrc and use FLG dotfiles version$NEWLINE"
-        printf "  2) Keep existing .zshrc (skip stowing zsh)$NEWLINE"
-        printf "  3) Show content of existing .zshrc first$NEWLINE"
-        printf "$PREFIX Enter your choice (1-3): "
-        
-        read -r choice
-        case $choice in
-            1)
-                local backup_name="$HOME/.zshrc.pre-flg-dots.$(date +%Y%m%d-%H%M%S)"
-                printf "$PREFIX Backing up existing .zshrc to $(basename "$backup_name")...$NEWLINE"
-                mv "$HOME/.zshrc" "$backup_name"
-                printf "$PREFIX Backup created successfully.$NEWLINE"
-                ;;
-            2)
-                printf "$PREFIX Keeping existing .zshrc, excluding zsh from stow packages.$NEWLINE"
-                stow_pkgs="hyprland kitty matugen quickshell walker wallpapers wallust waypaper"
-                ;;
-            3)
-                printf "$PREFIX Content of existing .zshrc:$NEWLINE"
-                printf "----------------------------------------$NEWLINE"
-                cat "$HOME/.zshrc"
-                printf "----------------------------------------$NEWLINE"
-                printf "$PREFIX Now choose: (1) Backup and replace, (2) Keep existing: "
-                read -r second_choice
-                case $second_choice in
-                    1)
-                        local backup_name="$HOME/.zshrc.pre-flg-dots.$(date +%Y%m%d-%H%M%S)"
-                        printf "$PREFIX Backing up existing .zshrc to $(basename "$backup_name")...$NEWLINE"
-                        mv "$HOME/.zshrc" "$backup_name"
-                        printf "$PREFIX Backup created successfully.$NEWLINE"
-                        ;;
-                    2)
-                        printf "$PREFIX Keeping existing .zshrc, excluding zsh from stow packages.$NEWLINE"
-                        stow_pkgs="hyprland kitty matugen quickshell walker wallpapers wallust waypaper"
-                        ;;
-                    *)
-                        printf "$PREFIX Invalid choice, keeping existing .zshrc.$NEWLINE"
-                        stow_pkgs="hyprland kitty matugen quickshell walker wallpapers wallust waypaper"
-                        ;;
-                esac
-                ;;
-            *)
-                printf "$PREFIX Invalid choice, keeping existing .zshrc.$NEWLINE"
-                stow_pkgs="hyprland kitty matugen quickshell walker wallpapers wallust waypaper"
-                ;;
-        esac
-    fi
+    # Handle zsh files manually
+    printf "$PREFIX Installing zsh configuration files...$NEWLINE"
+    local zsh_src="$rice_dir/zsh"
+    local zsh_files=(".zshrc" ".omp.json")
     
+    for file in "${zsh_files[@]}"; do
+        if [ -f "$HOME/$file" ]; then
+            printf "$PREFIX Existing $file found at $HOME/$file$NEWLINE"
+            printf "$PREFIX What would you like to do with $file?$NEWLINE"
+            printf "  1) Backup existing $file and use FLG dotfiles version$NEWLINE"
+            printf "  2) Keep existing $file$NEWLINE"
+            printf "  3) Show content of existing $file first$NEWLINE"
+            printf "$PREFIX Enter your choice (1-3): "
+            
+            read -r choice
+            case $choice in
+                1)
+                    local backup_name="$HOME/$file.pre-flg-dots.$(date +%Y%m%d-%H%M%S)"
+                    printf "$PREFIX Backing up existing $file to $(basename "$backup_name")...$NEWLINE"
+                    mv "$HOME/$file" "$backup_name"
+                    cp "$zsh_src/$file" "$HOME/$file"
+                    printf "$PREFIX $file backup created and FLG version installed.$NEWLINE"
+                    ;;
+                2)
+                    printf "$PREFIX Keeping existing $file.$NEWLINE"
+                    ;;
+                3)
+                    printf "$PREFIX Content of existing $file:$NEWLINE"
+                    printf "----------------------------------------$NEWLINE"
+                    cat "$HOME/$file"
+                    printf "----------------------------------------$NEWLINE"
+                    printf "$PREFIX Now choose: (1) Backup and replace, (2) Keep existing: "
+                    read -r second_choice
+                    case $second_choice in
+                        1)
+                            local backup_name="$HOME/$file.pre-flg-dots.$(date +%Y%m%d-%H%M%S)"
+                            printf "$PREFIX Backing up existing $file to $(basename "$backup_name")...$NEWLINE"
+                            mv "$HOME/$file" "$backup_name"
+                            cp "$zsh_src/$file" "$HOME/$file"
+                            printf "$PREFIX $file backup created and FLG version installed.$NEWLINE"
+                            ;;
+                        2)
+                            printf "$PREFIX Keeping existing $file.$NEWLINE"
+                            ;;
+                        *)
+                            printf "$PREFIX Invalid choice, keeping existing $file.$NEWLINE"
+                            ;;
+                    esac
+                    ;;
+                *)
+                    printf "$PREFIX Invalid choice, keeping existing $file.$NEWLINE"
+                    ;;
+            esac
+        else
+            # File doesn't exist, just copy it
+            printf "$PREFIX Installing $file...$NEWLINE"
+            cp "$zsh_src/$file" "$HOME/$file"
+            printf "$PREFIX $file installed successfully.$NEWLINE"
+        fi
+    done
+
     # Create some dirs in case they don't exist to make sure
     # that we don't have an ugly linking problem with stow
     mkdir -p "$HOME/.config/hypr"
